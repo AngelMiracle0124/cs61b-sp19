3a40e7b7c3e9fe1ca649028100a360c025031bf2
Qixuan Xing <xingqixuan@gmail.com>
2022-07-02 22:04:52 -0400

    finished

--- proj0/Body.java
+++ proj0/Body.java
@@ -1,2 +1,81 @@
-package PACKAGE_NAME;public class Body {
+public class Body {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    public Body(double xP, double yP, double xV,
+                double yV, double m, String img){
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+    public Body(Body b){
+        xxPos = b.xxPos;
+        yyPos = b.yyPos;
+        xxVel = b.xxVel;
+        yyVel = b.yyVel;
+        mass = b.mass;
+        imgFileName = b.imgFileName;
+    }
+    public double calcDistance(Body b){
+        /*return the distance between two Bodys*/
+        double xxDis = b.xxPos - xxPos;
+        double yyDis = b.yyPos - yyPos;
+        double r = Math.sqrt(Math.pow(xxDis,2) + Math.pow(yyDis,2));
+        return r;
+    }
+    public double calcForceExertedBy(Body b){
+        /* return the force exerted on this body by the given body */
+        double force = 6.67e-11 * b.mass * mass / Math.pow(calcDistance(b),2);
+        return force;
+    }
+    public double calcForceExertedByX(Body b){
+        /* return the force exerted in the X directions*/
+        double forcex = calcForceExertedBy(b) * (b.xxPos - xxPos) / calcDistance(b);
+        return forcex;
+    }
+    public double calcForceExertedByY(Body b){
+        /* return the force exerted in the Y directions*/
+        double forcey = calcForceExertedBy(b) * (b.yyPos - yyPos) / calcDistance(b);
+        return forcey;
+    }
+    public double calcNetForceExertedByX(Body[] blist){
+        /* return the net X force exerted by all bodies in that array upon the current Body*/
+        double netforceX = 0;
+        for (int i = 0; i < blist.length; i = i + 1){
+            if (!(this.equals(blist[i]))){
+                netforceX = netforceX + calcForceExertedByX(blist[i]);
+            }
+        }
+        return netforceX;
+    }
+
+    public double calcNetForceExertedByY(Body[] blist){
+        /* return the net Y force exerted by all bodies in that array upon the current Body*/
+        double netforceY = 0;
+        for (int i = 0;i < blist.length; i = i + 1){
+            if (!(this.equals(blist[i]))){
+                netforceY = netforceY + calcForceExertedByY(blist[i]);
+            }
+        }
+        return netforceY;
+    }
+    public void update(double dt, double fX, double fY){
+       double accx = fX / mass;
+       double accy = fY / mass;
+       xxVel = xxVel + dt * accx;
+       yyVel = yyVel + dt * accy;
+       xxPos = xxPos + dt * xxVel;
+       yyPos = yyPos + dt * yyVel;
+    }
+    public void draw(){
+        StdDraw.picture(xxPos, yyPos,  "./images/" + imgFileName);
+    }
+
 }
--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,2 +1,73 @@
-package PACKAGE_NAME;public class NBody {
+import java.awt.desktop.SystemEventListener;
+
+public class NBody {
+    public static double readRadius(String file) {
+        In in = new In(file);
+        int numofplanet = in.readInt(); /*first item in the file: the number of planet;*/
+        double radius = in.readDouble();/* second item in the file: radius*/
+        return radius;
+    }
+    public static Body[] readBodies(String file){
+        In in = new In(file);
+        int numofplanet = in.readInt(); /*first item in the file: the number of planet;*/
+        double r = in.readDouble(); /* second item in the file: radius*/
+        Body[] moreplanet = new Body[numofplanet]; /*declear a new Body[]*/
+        for (int i = 0; i < moreplanet.length; i ++){
+            double xP = in.readDouble();
+            double yP = in.readDouble();
+            double xV = in.readDouble();
+            double yV = in.readDouble();
+            double m = in.readDouble();
+            String img = in.readString();
+            moreplanet[i] = new Body(xP,yP, xV, yV, m, img);/*store all the planet info in this new Body[]*/
+        }
+        return moreplanet;
+    }
+
+    public static void main(String[] args) {
+        /*Collecting All Needed Input*/
+        double T = Double.parseDouble(args[0]);
+        double dt = Double.parseDouble(args[1]);
+        String filename = args[2];
+        double universeradius = readRadius(filename);
+        Body[] bodyinfo = readBodies(filename);
+
+
+        StdDraw.enableDoubleBuffering();/*enableDoubleBuffering(): This is a graphics technique to prevent flickering in the animation*/
+        int time = 0;
+        while (time < T){
+            double[] xForces = new double[bodyinfo.length];
+            double[] yForces = new double[bodyinfo.length];
+            for (int i = 0; i < bodyinfo.length; i ++){
+                xForces[i] = bodyinfo[i].calcNetForceExertedByX(bodyinfo);
+                yForces[i] = bodyinfo[i].calcNetForceExertedByY(bodyinfo);
+            }
+            for (int i = 0; i < bodyinfo.length; i ++){
+                bodyinfo[i].update(dt, xForces[i], yForces[i]);
+            }
+            /*Drawing the Background*/
+            String background = "images/starfield.jpg";
+            StdDraw.setScale(-universeradius, universeradius);
+            StdDraw.clear();
+            StdDraw.picture(0, 0, background);
+
+            /*Draw all of the Bodys.*/
+            for (int i = 0; i < bodyinfo.length; i ++){
+                bodyinfo[i].draw();
+                // System.out.println(i);
+            }
+            StdDraw.show();
+            StdDraw.pause(10);
+            dt += 1;
+
+        }
+        StdOut.printf("%d\n", bodyinfo.length);
+        StdOut.printf("%.2e\n", universeradius);
+        for (int i = 0; i < bodyinfo.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                    bodyinfo[i].xxPos, bodyinfo[i].yyPos, bodyinfo[i].xxVel,
+                    bodyinfo[i].yyVel, bodyinfo[i].mass, bodyinfo[i].imgFileName);
+        }
+
+    }
 }
