52161adb057b9b5a93dd72456b69fcae6a07a502
Qixuan Xing <xingqixuan@gmail.com>
2022-07-20 17:07:43 -0400

    delete main part

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -88,7 +88,7 @@ public class ArrayDeque<T>{
         items[nextfirst] = null;
         size = size - 1;
         if (items.length >= 16 && size < (items.length / 4)) {
-            resize(size - 1);
+            resize(size + 1);
         }
         return oldfirst;
     }
@@ -101,7 +101,7 @@ public class ArrayDeque<T>{
         items[nextlast] = null;
         size = size - 1;
         if (items.length >= 16 && size < (items.length / 4)) {
-            resize(size - 1);
+            resize(size + 1);
         }
         return oldlast;
     }
@@ -112,33 +112,33 @@ public class ArrayDeque<T>{
         int p = addone(nextfirst);
         return items[(index + p) % items.length];
     }
-    public static void main(String[] args) {
-        System.out.println("Running tests.\n");
-        ArrayDeque<Integer> ad1 = new ArrayDeque<Integer>();
-        ad1.addLast(0);
-        ad1.removeLast();
-        ad1.addFirst(2);
-        ad1.removeFirst();
-        ad1.addFirst(4);
-        ad1.removeFirst();
-        ad1.addFirst(6);
-        ad1.addFirst(7);
-        ad1.removeFirst();
-        ad1.removeFirst();
-        ad1.addLast(10);
-        ad1.addFirst(11);
-        ad1.addLast(12);
-        ad1.addLast(13);
-        ad1.addLast(14);
-        ad1.removeLast();
-        ad1.addLast(16);
-        ad1.addFirst(17);
-        ad1.addFirst(18);
-        ad1.addFirst(19);
-        ad1.addLast(20);
-        ad1.addFirst(21);
-        System.out.println(ad1.get(1));
-
-
-    }
+//    public static void main(String[] args) {
+//        System.out.println("Running tests.\n");
+//        ArrayDeque<Integer> ad1 = new ArrayDeque<Integer>();
+//        ad1.addLast(0);
+//        ad1.removeLast();
+//        ad1.addFirst(2);
+//        ad1.removeFirst();
+//        ad1.addFirst(4);
+//        ad1.removeFirst();
+//        ad1.addFirst(6);
+//        ad1.addFirst(7);
+//        ad1.removeFirst();
+//        ad1.removeFirst();
+//        ad1.addLast(10);
+//        ad1.addFirst(11);
+//        ad1.addLast(12);
+//        ad1.addLast(13);
+//        ad1.addLast(14);
+//        ad1.removeLast();
+//        ad1.addLast(16);
+//        ad1.addFirst(17);
+//        ad1.addFirst(18);
+//        ad1.addFirst(19);
+//        ad1.addLast(20);
+//        ad1.addFirst(21);
+//        System.out.println(ad1.get(1));
+//
+//
+//    }
 }
